e4daa1041daaa3e920452f6e827d0ecc05c42fe9
Jack Meeten <61755971+Jack-Meeten@users.noreply.github.com>
2022-12-14 11:54:15 +0000

    39 started

---
+++ Week9/Program39_TopTenScores/Program39_TopTenScores.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+//https://cplusplus.com/reference/fstream/
+
+int main()
+{
+    fstream scoresFile;
+
+
+    scoresFile.open("scores.txt");
+}
